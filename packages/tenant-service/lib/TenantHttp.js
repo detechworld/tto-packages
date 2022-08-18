191995228d892e0472a2c051e44e47337d662602
UladzislauVintsarevich <u.vintsarevich@paralect.com>
2022-08-18 17:02:20 +0400

    feat(tenant-service): Init Tenant service package

---
+++ packages/tenant-service/lib/TenantHttp.js
@@ -0,0 +1,78 @@
+import { HttpService } from '@detechworld/http-service';
+import { Singleton } from '@detechworld/toolbox';
+
+class TenantHttp extends Singleton {
+  http = HttpService.getInstance();
+
+  getTenant(tenantId) {
+    return this.http.get(`/tenant/${tenantId}`);
+  }
+
+  getNetworkTenant(tenantId) {
+    return this.http.get(`/api/network/tenants/${tenantId}`);
+  }
+
+  getNetworkTenants() {
+    return this.http.get(`/api/network/tenants/listing`);
+  }
+
+  updateTenantProfile(updatedProfile) {
+    return this.http.put(`/tenant/profile`, updatedProfile);
+  }
+
+  updateNetworkSettings(data) {
+    return this.http.put(`/tenant/network-settings`, data, {
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+  }
+
+  updateTenantSettings(form) {
+    return this.http.put(`/tenant/settings`, form, {
+      headers: {
+        'Content-Type': 'multipart/form-data'
+      }
+    });
+  }
+
+  createTenantResearchAttribute(researchAttribute) {
+    return this.http.post(`/tenant/research-attributes`, researchAttribute);
+  }
+
+  updateTenantResearchAttribute(researchAttribute) {
+    return this.http.put(`/tenant/research-attributes`, researchAttribute);
+  }
+
+  deleteTenantResearchAttribute(researchAttributeId) {
+    return this.http.delete_(`/tenant/research-attributes/${researchAttributeId}`);
+  }
+
+  postSignUp(data) {
+    return this.http.post(`/tenant/registry/sign-up`, data);
+  }
+
+  getSignUpRequests() {
+    return this.http.get(`/tenant/registry/sign-ups`);
+  }
+
+  approveSignUpRequest(username) {
+    return this.http.put(`/tenant/registry/sign-ups/approve`, { username });
+  }
+
+  rejectSignUpRequest(username) {
+    return this.http.put(`/tenant/registry/sign-ups/reject`, { username });
+  }
+
+  addAdminTenant({ tx }) {
+    return this.http.put(`/tenant/admins/add`, { tx });
+  }
+
+  removeAdminTenant({ tx }) {
+    return this.http.put(`/tenant/admins/remove`, { tx });
+  }
+}
+
+export {
+  TenantHttp
+};
---
+++ packages/tenant-service/lib/TenantService.js
@@ -0,0 +1,65 @@
+import { Singleton } from '@detechworld/toolbox';
+import { TenantHttp } from './TenantHttp';
+import { BlockchainService } from '@detechworld/blockchain-service';
+
+class TenantService extends Singleton {
+  tenantHttp = TenantHttp.getInstance();
+  blockchainService = BlockchainService.getInstance();
+
+  getTenant(tenantId) {
+    return this.tenantHttp.getTenant(tenantId);
+  }
+
+  getNetworkTenant(tenantId) {
+    return this.tenantHttp.getNetworkTenant(tenantId);
+  }
+
+  getNetworkTenants() {
+    return this.tenantHttp.getNetworkTenants();
+  }
+
+  updateTenantProfile(updatedProfile) {
+    return this.tenantHttp.updateTenantProfile(updatedProfile);
+  }
+
+  updateNetworkSettings(data) {
+    return this.tenantHttp.updateNetworkSettings(data);
+  }
+
+  updateTenantSettings(form) {
+    return this.tenantHttp.updateTenantSettings(form);
+  }
+
+  createTenantResearchAttribute(researchAttribute) {
+    return this.tenantHttp.createTenantResearchAttribute(researchAttribute);
+  }
+
+  updateTenantResearchAttribute(researchAttribute) {
+    return this.tenantHttp.updateTenantResearchAttribute(researchAttribute);
+  }
+
+  deleteTenantResearchAttribute(researchAttribute) {
+    return this.tenantHttp.deleteTenantResearchAttribute(researchAttribute);
+  }
+
+  postSignUp(data) {
+    return this.tenantHttp.postSignUp(data);
+  }
+
+  getSignUpRequests() {
+    return this.tenantHttp.getSignUpRequests();
+  }
+
+  approveSignUpRequest(username) {
+    return this.tenantHttp.approveSignUpRequest(username);
+  }
+
+  rejectSignUpRequest(username) {
+    return this.tenantHttp.rejectSignUpRequest(username);
+  }
+
+}
+
+export {
+  TenantService
+};
